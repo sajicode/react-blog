3e6ae4a8d3e83f619491a6478c03e15dec6579d2
sajicode <sajioloye@gmail.com>
2018-12-10 12:22:52 +0100

    set up webpack for prod and for separate css files

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,64 @@
 // entry -> output
 const devMode = process.env.NODE_ENV !== 'production';
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-	mode: devMode ? 'development' : 'production',
-	entry: {
-		app: [ '@babel/polyfill', './src/app.js' ]
-	},
-	output: {
-		path: path.join(__dirname, 'public'),
-		filename: 'bundle.js'
-	},
-	target: 'web',
-	module: {
-		rules: [
-			{
-				use: {
-					loader: 'babel-loader'
+module.exports = (env) => {
+	const isProduction = env === 'production';
+
+	console.log('env', env);
+
+	return {
+		mode: devMode ? 'development' : 'production',
+		entry: {
+			app: [ '@babel/polyfill', './src/app.js' ]
+		},
+		output: {
+			path: path.join(__dirname, 'public', 'dist'),
+			filename: 'bundle.js'
+		},
+		target: 'web',
+		module: {
+			rules: [
+				{
+					use: {
+						loader: 'babel-loader'
+					},
+					test: /\.js$/,
+					include: [ path.resolve(__dirname, 'src') ],
+					exclude: /node_modules/
 				},
-				test: /\.js$/,
-				include: [ path.resolve(__dirname, 'src') ],
-				exclude: /node_modules/
-			},
-			{
-				test: /\.s?css?/,
-				use: [ 'style-loader', 'css-loader', 'sass-loader' ]
-			}
-		]
-	},
-	devtool: 'cheap-module-eval-source-map',
-	devServer: {
-		contentBase: path.join(__dirname, 'public'),
-		historyApiFallback: true
-	}
+				{
+					test: /\.s?css?/,
+					use: [
+						'style-loader',
+						MiniCssExtractPlugin,
+						{
+							loader: 'css-loader',
+							options: {
+								sourceMap: true
+							}
+						},
+						{
+							loader: 'sass-loader',
+							options: {
+								sourceMap: true
+							}
+						}
+					]
+				}
+			]
+		},
+		plugins: [
+			new MiniCssExtractPlugin({
+				filename: 'styles.css'
+			})
+		],
+		devtool: isProduction ? 'source-map' : 'inline-source-map',
+		devServer: {
+			contentBase: path.join(__dirname, 'public'),
+			historyApiFallback: true,
+			publicPath: '/dist/'
+		}
+	};
 };
