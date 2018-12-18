69199412d75ee28cba60b2fed6e0ed901567b7b4
sajicode <sajioloye@gmail.com>
2018-12-18 19:56:46 +0100

    modify store to work with redux-thunk

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,9 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import postsReducer from '../reducers/posts';
 import filtersReducer from '../reducers/filters';
+import thunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default () => {
 	// create store
@@ -9,7 +12,8 @@ export default () => {
 			posts: postsReducer,
 			filters: filtersReducer
 		}),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+		composeEnhancers(applyMiddleware(thunk))
+		// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 	);
 
 	return store;
